fc52e4d66dfe0469ca2973c406edd23fd7272f33
Wilson Junior <wilson.junior@merx.tech>
2023-03-19 20:46:32 -0300

    feat: add config react navigation

---
+++ src/presentation/pages/Settings/index.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "native-base";
+
+export function Settings() {
+  return (
+    <Box>
+      <Text>Settings</Text>
+    </Box>
+  )
+}
---
+++ src/presentation/pages/VariableExpenses/index.tsx
@@ -0,0 +1,39 @@
+import {
+  Box,
+  Center,
+  FlatList,
+  Heading,
+  Spacer,
+} from 'native-base'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import { Rent } from '../../components/Rent'
+import { useLocales } from '../../../hooks/locales'
+
+const data = [{ name: 'teste1' }, { name: 'teste2' } ]
+
+function Separator() {
+  return <Spacer height={4} />
+}
+
+export function VariableExpenses() {
+  const { translate } = useLocales()
+
+  return (
+    <Box marginTop={getStatusBarHeight() + 10}>
+      <Heading fontSize="xl" p="4" pb="3">
+        {translate('save')}
+      </Heading>
+
+      <Center width="100%">
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Rent />}
+          width="100%"
+          padding={4}
+          ItemSeparatorComponent={Separator}
+        />
+      </Center>
+    </Box>
+  )
+}
