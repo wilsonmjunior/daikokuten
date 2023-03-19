eedc8cc981ddbea2845aaa9b35066aab80f7e47d
Wilson Junior <wilson.junior@merx.tech>
2023-03-19 00:40:52 -0300

    feat: create base home

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,12 +1,12 @@
-import { Center, VStack } from "native-base";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { useEffect } from "react";
+import { HomePresentation } from "./home.presentation";
 
 export function Home() {
+  useEffect(() => {
+    // buscar dados
+  }, [])
+
   return (
-    <VStack space={4} alignItems="center" marginTop={getStatusBarHeight()}>
-      <Center w="64" h="20" bg="indigo.300" rounded="md" shadow={3} />
-      <Center w="64" h="20" bg="indigo.500" rounded="md" shadow={3} />
-      <Center w="64" h="20" bg="indigo.700" rounded="md" shadow={3} />
-    </VStack>
+    <HomePresentation />
   )
 }
