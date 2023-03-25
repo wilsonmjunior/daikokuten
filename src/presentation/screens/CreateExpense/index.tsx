10d9b170acaed86c45a0490a896854beb3d667f7
Wilson Junior <wilson.junior@merx.tech>
2023-03-25 00:11:10 -0300

    fix: rename folders

--- src/presentation/screens/CreateExpense/index.tsx
+++ src/presentation/screens/CreateExpense/index.tsx
@@ -1,10 +1,8 @@
 import { Box } from 'native-base'
-import React from 'react'
 
-import { IExpense } from '../../../infra/models/Expense'
+import { IExpense } from '../../../infra/types/Expense'
 
 import { createExpense } from '../../../infra/services/expenses'
-import { Expense } from '../../../dtos/Expense'
 
 export function CreateExpense() {
   const handleCreateExpense = async () => {
